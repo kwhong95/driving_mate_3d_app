502c1090cd16775b09a3c259109b70670071fc10
kwhong95 <kwhong95@kakao.com>
2021-07-26 23:43:27 +0900

    [DEV]Create Camera Controler

---
+++ src/View/Camera.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+import { useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const Camera = () => {
+  const { camera, gl } = useThree()
+
+  useEffect(
+      () => {
+        const controls  = new OrbitControls(camera, gl.domElement)
+        controls.minDistance = 3
+        controls.maxDistance = 20
+
+        return () => controls.dispose()
+
+  }, [ camera, gl ])
+
+  return null
+}
+
+export default Camera
