3b136ce0de8e586e1905b4a8eb5183ed912d9125
kwhong95 <kwhong95@kakao.com>
2021-07-26 17:30:15 +0900

    [DEV]Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+     <>Interface</>
+     <>3D View</>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
